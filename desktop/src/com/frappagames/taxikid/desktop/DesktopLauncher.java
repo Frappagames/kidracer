00a68564878bbb6643c54e4b8f16f29e97112635
Jérémy MOREAU <jeremy.moreau@gfpfrance.com>
2017-02-01 17:18:09 +0100

    Initialize LibGdx project

---
+++ desktop/src/com/frappagames/taxikid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frappagames.taxikid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frappagames.taxikid.TaxiKid;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaxiKid(), config);
+	}
+}
